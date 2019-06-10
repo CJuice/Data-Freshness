f5d2b67b0a67aa87cfaa9e843172433278b27afa
CJuice <CJuice@users.noreply.github.com>
2019-06-10 14:55:54 -0400

    Socrata response json stored in dataset objects

--- doit_DataFreshness_MainProcess.py
+++ doit_DataFreshness_MainProcess.py
@@ -8,30 +8,51 @@ def main():
     # IMPORTS
     import configparser
     import sodapy
+    import json
+    import itertools
+
     from DataFreshness.doit_DataFreshness_Utility import Utility
     from DataFreshness.doit_DataFreshness_DatasetSocrata import DatasetSocrata
     import DataFreshness.doit_DataFreshness_Variables as var
+    import os
 
     # VARIABLES
     socrata_class_objects_list = []
+    socrata_counter = itertools.count()
+    arcgisonline_counter = itertools.count()
+    credentials_parser = Utility.setup_config(cfg_file=var.credentials_config_file_path)
 
     # CLASSES
     # FUNCTIONS
     # FUNCTIONALITY
-    # TODO:
-    credentials_parser = Utility.setup_config(cfg_file=var.credentials_config_file_path)
-    socrata_client = Utility.create_socrata_client(domain=var.md_open_data_domain,
-                                                   app_token=credentials_parser["app_token"],
-                                                   username=credentials_parser["username"],
-                                                   password=credentials_parser["password"])
-    response_socrata = Utility.request_GET(url=var.md_open_data_url)
-    response_socrata_json = response_socrata.json()
 
-    for json_obj in response_socrata_json:
-        socrata_obj = DatasetSocrata(dataset_json=json_obj)
-        socrata_obj.assign_data_json_to_class_values()
-        socrata_obj.extract_four_by_four()
-        socrata_obj.build_metadata_url()
+    # TODO: Need a socrata client for making requests for protected information
+    client_socrata = Utility.create_socrata_client(domain=var.md_open_data_domain,
+                                                   app_token=credentials_parser["SOCRATA"]["app_token"],
+                                                   username=credentials_parser["SOCRATA"]["username"],
+                                                   password=credentials_parser["SOCRATA"]["password"])
+
+    # TODO: Get the data.json from Socrata so have an inventory of all public datasets
+    response_socrata = Utility.request_GET(url=var.md_socrata_data_json)
+    try:
+        response_socrata_json = response_socrata.json()
+    except json.decoder.JSONDecodeError as jde:
+        print(f"Error decoding socrata response to json: {jde}")
+        exit()
+    else:
+        datasets_json = response_socrata_json.get("dataset", {})
+
+    # TODO: Need to create Socrata Dataset objects from the datasets json, and store objects in list for use.
+    for json_obj in datasets_json:
+        print(next(socrata_counter))
+        dataset_socrata = DatasetSocrata(dataset_json=json_obj)
+        dataset_socrata.assign_data_json_to_class_values()
+        dataset_socrata.extract_four_by_four()
+        dataset_socrata.build_metadata_url()
+        socrata_class_objects_list.append(dataset_socrata)
+
+    # TODO: 
+
 
 
 if __name__ == "__main__":
