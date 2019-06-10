e8f077d1e584d3155a8e9cedebd7f099bb7f7f23
CJuice <CJuice@users.noreply.github.com>
2019-06-10 12:28:43 -0400

    continued setup

--- DatasetArcGISOnline.py
+++ DatasetArcGISOnline.py
@@ -1,2 +1,5 @@
+"""
+
+"""
 class DatasetArcGISOnline:
     pass
--- MainProcess.py
+++ MainProcess.py
@@ -1,3 +1,11 @@
 """
 Main processing script coordinating all functionality for completing data freshness process.
 """
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
