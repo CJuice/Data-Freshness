fa32fafcb3f8347bbf995b0672b3f6ec924f0818
CJuice <CJuice@users.noreply.github.com>
2019-06-10 14:51:04 -0400

    correct path to credentials file

--- doit_DataFreshness_Variables.py
+++ doit_DataFreshness_Variables.py
@@ -2,7 +2,8 @@
 File designated for process variables in order to centralize variables, and de-clutter main script.
 """
 
-credentials_config_file_path = r"DataFreshness/doit_DataFreshness_Credentials/doit_DataFreshness_Credentials.cfg"
+credentials_config_file_path = r"doit_DataFreshness_Credentials/doit_DataFreshness_Credentials.cfg"
 
 md_open_data_domain = r"opendata.maryland.gov"
 md_open_data_url = fr"https://{md_open_data_domain}"
+md_socrata_data_json = f"{md_open_data_url}/data.json"
