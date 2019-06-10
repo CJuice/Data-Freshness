eb5af21e3e553d0fec73ba27065a918c4bda68d7
CJuice <CJuice@users.noreply.github.com>
2019-06-10 12:56:14 -0400

    Update MainProcess.py

--- MainProcess.py
+++ MainProcess.py
@@ -6,14 +6,16 @@ Main processing script coordinating all functionality for completing data freshn
 def main():
     # IMPORTS
     import requests
-    import Utility
+    import
+    import Utility as doit_util
+    from DatasetSocrata import DatasetSocrata as socrat
 
     # VARIABLES
     # CLASSES
     # FUNCTIONS
     # FUNCTIONALITY
+    doit_util.request_GET(url=socrat.)
 
-
-
+Data-Freshness/Utility.py
 if __name__ == "__main__":
     main()
