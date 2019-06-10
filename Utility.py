1d45bf5967f2ccc3170c30c80cc70257e2b62ee4
CJuice <CJuice@users.noreply.github.com>
2019-06-10 12:47:51 -0400

    adding request func

--- Utility.py
+++ Utility.py
@@ -1,5 +1,20 @@
 """
 
 """
+import requests
+
+
 class Utility:
-    pass
+
+    @staticmethod
+    def request_GET(url, params=None):
+        if params is None:
+            params = {}
+
+        try:
+            response = requests.get(url=url, params=params)
+        except Exception as e:
+            print(f"Error with request to {url}. Code {response.status_code}. Error:{e}")
+            exit()
+        else:
+            return response
