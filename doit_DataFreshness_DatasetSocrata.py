cbd004782c5e06cd62ae99231250aca5fda5927d
CJuice <CJuice@users.noreply.github.com>
2019-06-10 15:26:24 -0400

    new functions and refactored json parsing
    was storing entire json object in object and then also storing all attributes assigned to instance object attributes. To eliminate unnecessary bloat I  stopped storing the json raw and moved to just storing the assigned values to the object

--- doit_DataFreshness_DatasetSocrata.py
+++ doit_DataFreshness_DatasetSocrata.py
@@ -10,14 +10,15 @@ class DatasetSocrata:
 
     """
 
-    def __init__(self, dataset_json: dict):
+    def __init__(self):
         """
 
         :param dataset_json:
         """
         self.access_level = None
+        self.asset_inventory_url = None
         self.contact_point_dict = None
-        self.dataset_json = dataset_json
+        # self.dataset_json = dataset_json
         self.description = None
         self.distribution_list = None
         self.four_by_four = None
@@ -28,28 +29,29 @@ class DatasetSocrata:
         self.metadata_url = None
         self.modified = None
         self.publisher_dict = None
+        self.resource_url = None
         self.theme_list = None
         self.title = None
         self.type = None
 
-    def assign_data_json_to_class_values(self):
+    def assign_data_json_to_class_values(self, dataset_json: dict):
         """
 
         :return:
         """
-        self.access_level = self.dataset_json.get("accessLevel", None)
-        self.contact_point_dict = self.dataset_json.get("contactPoint", None)
-        self.description = self.dataset_json.get("description", None)
-        self.distribution_list = self.dataset_json.get("distribution", None)
-        self.identifier_url = self.dataset_json.get("identifier", None)
-        self.issued = self.dataset_json.get("issued", None)
-        self.keyword_list = self.dataset_json.get("keyword", None)
-        self.landing_page = self.dataset_json.get("landingPage", None)
-        self.modified = self.dataset_json.get("modified", None)
-        self.publisher_dict = self.dataset_json.get("publisher", None)
-        self.type = self.dataset_json.get("@type", None)
-        self.theme_list = self.dataset_json.get("theme", None)
-        self.title = self.dataset_json.get("title", None)
+        self.access_level = dataset_json.get("accessLevel", None)
+        self.contact_point_dict = dataset_json.get("contactPoint", None)
+        self.description = dataset_json.get("description", None)
+        self.distribution_list = dataset_json.get("distribution", None)
+        self.identifier_url = dataset_json.get("identifier", None)
+        self.issued = dataset_json.get("issued", None)
+        self.keyword_list = dataset_json.get("keyword", None)
+        self.landing_page = dataset_json.get("landingPage", None)
+        self.modified = dataset_json.get("modified", None)
+        self.publisher_dict = dataset_json.get("publisher", None)
+        self.type = dataset_json.get("@type", None)
+        self.theme_list = dataset_json.get("theme", None)
+        self.title = dataset_json.get("title", None)
         return None
 
     def build_metadata_url(self):
@@ -58,6 +60,22 @@ class DatasetSocrata:
         :return:
         """
         self.metadata_url = f"{var.md_open_data_url}/api/views/{self.four_by_four}.json"
+        return None
+
+    def build_resource_url(self):
+        """
+
+        :return:
+        """
+        self.resource_url = f"{var.md_open_data_url}/resource/{self.four_by_four}.json"
+        return None
+
+    def build_asset_inventory_url(self, fourbyfour):
+        """
+
+        :return:
+        """
+        self.asset_inventory_url = f"{var.md_open_data_url}/resource/{fourbyfour}.json?u_id={cur_identifier}&$$app_token={socrata_app_token}"
         return None
 
     def extract_four_by_four(self):
