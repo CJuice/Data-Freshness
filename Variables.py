d58c375a3f60b64eb699647ff8dca2faa2c2d4b7
CJuice <CJuice@users.noreply.github.com>
2019-06-10 13:22:51 -0400

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,3 +1,5 @@
 """
 File designated for process variables in order to centralize variables, and de-clutter main script.
 """
+
+credentials_config_file_path = r"DataFreshness/Credentials/Credentials.cfg"
