1ccaab2b410bfc84ab110fcd0ca65f31cd895bc6
CJuice <CJuice@users.noreply.github.com>
2019-06-10 13:22:43 -0400

    Update Utility.py

--- Utility.py
+++ Utility.py
@@ -2,6 +2,8 @@
 
 """
 import requests
+import configparser
+from sodapy import Socrata
 
 
 class Utility:
@@ -18,3 +20,30 @@ class Utility:
             exit()
         else:
             return response
+
+    @staticmethod
+    def setup_config(cfg_file: str) -> configparser.ConfigParser:
+        """
+        Instantiate the parser for accessing a config file.
+        :param cfg_file: config file to access
+        :return:
+        """
+        cfg_parser = configparser.ConfigParser()
+        cfg_parser.read(filenames=cfg_file)
+        return cfg_parser
+
+    @staticmethod
+    def create_socrata_client(username:str, password: str, app_token: str, domain: str, dataset_key: str) -> Socrata:
+        """
+        Create and return a Socrata client for use.
+
+        NOTE_1: It seems absolutely essential the the domain be a domain and not a url; 'https://opendata.maryland.gov'
+            will not substitute for 'opendata.maryland.gov'.
+
+        :param cfg_parser: config file parser
+        :param dataset_key: the section key of interest
+        :param domain: domain for maryland open data portal.
+        :return: Socrata connection client
+        """
+
+        return Socrata(domain=domain, app_token=app_token, username=username, password=password)
