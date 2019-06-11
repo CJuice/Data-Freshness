76f3db5444ada755b86cff98e052e872dd0f5b1a
CJuice <CJuice@users.noreply.github.com>
2019-06-11 10:39:15 -0400

    added func for requesting all records despite length

--- doit_DataFreshness_DatasetSocrata.py
+++ doit_DataFreshness_DatasetSocrata.py
@@ -3,7 +3,8 @@
 """
 import os
 import DataFreshness.doit_DataFreshness_Variables as var
-
+from sodapy import Socrata
+import time
 
 class DatasetSocrata:
     """
@@ -12,6 +13,7 @@ class DatasetSocrata:
 
     # Class attributes available to all instances
     SOCRATA_CLIENT = None
+    LIMIT_MAX_AND_OFFSET = 10000
 
     # Methods
     def __init__(self):
@@ -19,8 +21,8 @@ class DatasetSocrata:
 
         :param dataset_json:
         """
+        # DATA.JSON SOURCED VALUES
         self.access_level = None
-        self.asset_inventory_url = None
         self.contact_point_dict = None
         self.description = None
         self.distribution_list = None
@@ -36,6 +38,9 @@ class DatasetSocrata:
         self.theme_list = None
         self.title = None
         self.type = None
+
+        # ASSET INVENTORY SOURCED VALUES
+
 
     def assign_data_json_to_class_values(self, dataset_json: dict):
         """
@@ -73,7 +78,6 @@ class DatasetSocrata:
         self.resource_url = f"{var.md_open_data_url}/resource/{self.four_by_four}.json"
         return None
 
-
     def extract_four_by_four(self):
         """
 
@@ -81,3 +85,55 @@ class DatasetSocrata:
         """
         self.four_by_four = os.path.basename(self.landing_page)
         return None
+
+    @staticmethod
+    def create_socrata_client(domain: str, app_token: str, username:str, password: str) -> Socrata:
+        """
+        Create and return a Socrata client for use.
+
+        NOTE_1: It seems absolutely essential the the domain be a domain and not a url; 'https://opendata.maryland.gov'
+            will not substitute for 'opendata.maryland.gov'.
+
+        :param domain: domain for maryland open data portal.
+        :param app_token: application token for throttling limitations
+        :param username: account username
+        :param password: password for account
+        :return: Socrata connection client
+        """
+
+        return Socrata(domain=domain, app_token=app_token, username=username, password=password)
+
+    @staticmethod
+    def request_and_aggregate_all_socrata_records(client: Socrata, fourbyfour: str, limit_max_and_offset: int = LIMIT_MAX_AND_OFFSET) -> list:
+        """
+
+        :param client:
+        :param fourbyfour:
+        :param limit_max_and_offset:
+        :return:
+        """
+        more_records_exist_than_response_limit_allows = True
+        total_record_count = 0
+        record_offset_value = 0
+        master_list_of_dicts = []
+        while more_records_exist_than_response_limit_allows:
+            request_cycle_record_count = 0
+            response = client.get(dataset_identifier=fourbyfour,
+                                  content_type="json",
+                                  limit=limit_max_and_offset,
+                                  offset=record_offset_value)
+            master_list_of_dicts.append(response)
+            number_of_records_returned = len(response)
+            request_cycle_record_count += number_of_records_returned
+            total_record_count += number_of_records_returned
+
+            # Any cycle_record_count that equals the max limit indicates another request is needed
+            if request_cycle_record_count == limit_max_and_offset:
+
+                # Give Socrata servers small interval before requesting more
+                time.sleep(0.2)
+                record_offset_value = request_cycle_record_count + record_offset_value
+            else:
+                more_records_exist_than_response_limit_allows = False
+
+        return master_list_of_dicts
