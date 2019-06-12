62a89ed12c50f8cb771ecb297dc3d5f22834abd2
CJuice <CJuice@users.noreply.github.com>
2019-06-12 11:07:36 -0400

    added time taken func

--- doit_DataFreshness_Utility.py
+++ doit_DataFreshness_Utility.py
@@ -4,6 +4,7 @@
 import requests
 import configparser
 from sodapy import Socrata
+import time
 
 
 class Utility:
@@ -32,4 +33,13 @@ class Utility:
         cfg_parser.read(filenames=cfg_file)
         return cfg_parser
 
+    @staticmethod
+    def calculate_time_taken(start_time: float) -> float:
+        """
+        Calculate the time difference between now and the value passed as the start time
+
+        :param start_time: Time value representing start of processing
+        :return: Difference value between start time and current time
+        """
+        return (time.time() - start_time)
 
