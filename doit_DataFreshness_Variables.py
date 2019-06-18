0541d33c48c83f81a5c40fc6be10f4500cda3cd9
CJuice <CJuice@users.noreply.github.com>
2019-06-18 14:40:06 -0400

    Update doit_DataFreshness_Variables.py

--- doit_DataFreshness_Variables.py
+++ doit_DataFreshness_Variables.py
@@ -2,22 +2,11 @@
 File designated for process variables in order to centralize variables, and de-clutter main script.
 """
 import time
+
+# NOT DERIVED
+all_map_layers = "All map layers from MD iMAP are in the process of being surveyed to determine this information."
+better_metadata_needed = "Better Metadata Needed."
 credentials_config_file_path = r"doit_DataFreshness_Credentials/doit_DataFreshness_Credentials.cfg"
-process_initiation_datetime_in_seconds = float(round(time.time()))
-number_of_seconds_in_a_day = 86400
-md_open_data_domain = r"opendata.maryland.gov"
-md_open_data_url = f"https://{md_open_data_domain}"
-md_socrata_data_json_url = f"{md_open_data_url}/data.json"
-md_socrata_profile_url = "{root}/profile/{user_four_by_four}"
-null_string = "NULL"
-other_update_frequency = "Other Update Frequency - If frequency isn't included in list above, please describe it here."
-please_describe_below = "Other (Please Describe Below)"
-# output_report_headers = ["Dataset Name", "Link", "Agency Performing Data Updates", "Owner", "Data Provided By",
-#                          "Source URL", "User who Made Last Update", "Update Frequency",
-#                          "Date of Most Recent Data Change", "Days Since Last Data Update",
-#                          "Date of Most Recent View Change in Data or Metadata", "Updated Recently Enough",
-#                          "Number of Rows", "Tags Keywords", "Column Names", "Missing Metadata Fields",
-#                          "Portal", "Category"]
 dataframe_to_header_mapping_for_output = {"Dataset Name": "title", "Link": "landing_page",
                                           "Agency Performing Data Updates": "state_agency_performing_data_updates",
                                           "Owner": "owner",
@@ -34,28 +23,6 @@ dataframe_to_header_mapping_for_output = {"Dataset Name": "title", "Link": "land
                                           "Tags Keywords": "keyword_tags_string", "Column Names": "column_names_string",
                                           "Missing Metadata Fields": "missing_metadata_fields", "Portal": "portal",
                                           "Category": "category_string"}
-
-# TODO: dictionary of columns and their types for pandas.
-# output_report_headers_column_types = {"Dataset Name": str, "Link": str, "Agency Performing Data Updates": str,
-#                                       "Owner": str,
-#                                       "Data Provided By": str, "Source URL": str, "User who Made Last Update": str,
-#                                       "Update Frequency": str, "Date of Most Recent Data Change": "datetime",
-#                                       "Days Since Last Data Update": int,
-#                                       "Date of Most Recent View Change in Data or Metadata": "datetime",
-#                                       "Updated Recently Enough": str, "Number of Rows": int, "Tags Keywords": str,
-#                                       "Column Names": str, "Missing Metadata Fields": str, "Portal": str,
-#                                       "Category": str}
-output_excel_file_path = r"Docs\Socrata_data_output.xlsx"
-output_excel_sheetname = "The Data Nasty"
-updated_enough_affirmative = "Yes"
-updated_enough_negative = "No"
-better_metadata_needed = "Better Metadata Needed."
-all_map_layers = "All map layers from MD iMAP are in the process of being surveyed to determine this information."
-whether_dataset = "Whether dataset is up to date cannot be calculated until the Department of Information Technology collects metadata on update frequency."
-update_frequency_missing = "Update frequency metadata are missing. Dataset owner should add metadata to resolve this issue."
-metadata_missing = "Metadata on update frequency are missing. Dataset owner should provide this information to resolve this issue."
-evaluation_difficult = f"{updated_enough_affirmative}. The data are updated as needed, which makes evaluation difficult. As an approximate measure, this dataset is evaluated as updated recently enough because it has been updated in the past month."
-
 expected_socrata_asset_inventory_json_keys_dict = {'api_endpoint': 'API Endpoint',
                                                    'category': 'Category',
                                                    'contactemail': 'Contact Email',
@@ -85,3 +52,33 @@ expected_socrata_asset_inventory_json_keys_dict = {'api_endpoint': 'API Endpoint
                                                    'u_id': 'Dataset ID',
                                                    'update_frequency': 'Update Frequency',
                                                    'visits': 'Visits'}
+md_open_data_domain = r"opendata.maryland.gov"
+md_socrata_profile_url = "{root}/profile/{user_four_by_four}"
+metadata_missing = "Metadata on update frequency are missing. Dataset owner should provide this information to resolve this issue."
+null_string = "NULL"
+number_of_seconds_in_a_day = 86400
+other_update_frequency = "Other Update Frequency - If frequency isn't included in list above, please describe it here."
+output_excel_file_path = r"Docs\Socrata_data_output.xlsx"
+output_excel_sheetname = "The Data Nasty"
+please_describe_below = "Other (Please Describe Below)"
+process_initiation_datetime_in_seconds = float(round(time.time()))
+update_frequency_missing = "Update frequency metadata are missing. Dataset owner should add metadata to resolve this issue."
+updated_enough_affirmative = "Yes"
+updated_enough_negative = "No"
+whether_dataset = "Whether dataset is up to date cannot be calculated until the Department of Information Technology collects metadata on update frequency."
+
+# DERIVED
+evaluation_difficult = f"{updated_enough_affirmative}. The data are updated as needed, which makes evaluation difficult. As an approximate measure, this dataset is evaluated as updated recently enough because it has been updated in the past month."
+md_open_data_url = f"https://{md_open_data_domain}"
+md_socrata_data_json_url = f"{md_open_data_url}/data.json"
+
+# TODO: dictionary of columns and their types for pandas. Does this matter?
+# output_report_headers_column_types = {"Dataset Name": str, "Link": str, "Agency Performing Data Updates": str,
+#                                       "Owner": str,
+#                                       "Data Provided By": str, "Source URL": str, "User who Made Last Update": str,
+#                                       "Update Frequency": str, "Date of Most Recent Data Change": "datetime",
+#                                       "Days Since Last Data Update": int,
+#                                       "Date of Most Recent View Change in Data or Metadata": "datetime",
+#                                       "Updated Recently Enough": str, "Number of Rows": int, "Tags Keywords": str,
+#                                       "Column Names": str, "Missing Metadata Fields": str, "Portal": str,
+#                                       "Category": str}
