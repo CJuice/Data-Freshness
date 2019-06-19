bee87b4a0ca033170e4645e8870f943915712665
CJuice <CJuice@users.noreply.github.com>
2019-06-19 14:24:03 -0400

    data catalog attributes created and json vals assigned

--- doit_DataFreshness_DatasetAGOL.py
+++ doit_DataFreshness_DatasetAGOL.py
@@ -15,16 +15,98 @@ class DatasetAGOL:
     SORT_FIELD = 'title'
 
     def __init__(self):
-        pass
+        self.access = None
+        self.access_information = None
+        self.app_categories = None
+        self.average_rating = None
+        self.banner = None
+        self.categories = None
+        self.content_status = None
+        self.created = None
+        self.culture = None
+        self.description = None
+        self.documentation = None
+        self.extent = None
+        self.group_designations = None
+        self.guid = None
+        self.id = None
+        self.industries = None
+        self.languages = None
+        self.large_thumbnail = None
+        self.license_info = None
+        self.listed = None
+        self.modified = None
+        self.name = None
+        self.number_of_comments = None
+        self.number_of_ratings = None
+        self.number_of_views = None
+        self.org_id = None
+        self.owner = None
+        self.properties = None
+        self.proxy_filter = None
+        self.score_completeness = None
+        self.screenshots = None
+        self.size = None
+        self.snippet = None
+        self.spatial_reference = None
+        self.tags = None
+        self.thumbnail = None
+        self.title = None
+        self.type_ = None
+        self.type_keywords = None
+        self.url = None
 
-    def build_arcgis_request_data_dict(self, start_num: int = None) -> dict:
-        return {
-            'q': DatasetAGOL.OWNER,
-            'num': DatasetAGOL.RECORD_LIMIT,
-            'start': start_num,
-            'sortField': DatasetAGOL.SORT_FIELD,
-            'f': 'json'
-        }
+
+    def assign_data_catalog_json_to_class_values(self, data_json):
+        self.access = data_json.get("access", None)
+        self.access_information = data_json.get("accessInformation", None)
+        self.app_categories = data_json.get("appCategories", None)
+        self.average_rating = data_json.get("avgRating", None)
+        self.banner = data_json.get("banner", None)
+        self.categories = data_json.get("categories", None)
+        self.content_status = data_json.get("contentStatus", None)
+        self.created = data_json.get("created", None)
+        self.culture = data_json.get("culture", None)
+        self.description = data_json.get("description", None)
+        self.documentation = data_json.get("documentation", None)
+        self.extent = data_json.get("extent", None)
+        self.group_designations = data_json.get("groupDesignations", None)
+        self.guid = data_json.get("guid", None)
+        self.id = data_json.get("id", None)
+        self.industries = data_json.get("industries", None)
+        self.languages = data_json.get("languages", None)
+        self.large_thumbnail = data_json.get("largeThumbnail", None)
+        self.license_info = data_json.get("licenseInfo", None)
+        self.listed = data_json.get("listed", None)
+        self.modified = data_json.get("modified", None)
+        self.name = data_json.get("name", None)
+        self.number_of_comments = data_json.get("numComments", None)
+        self.number_of_ratings = data_json.get("numRatings", None)
+        self.number_of_views = data_json.get("numViews", None)
+        self.org_id = data_json.get("orgId", None)
+        self.owner = data_json.get("owner", None)
+        self.properties = data_json.get("properties", None)
+        self.proxy_filter = data_json.get("proxyFilter", None)
+        self.score_completeness = data_json.get("scoreCompleteness", None)
+        self.screenshots = data_json.get("screenshots", None)
+        self.size = data_json.get("size", None)
+        self.snippet = data_json.get("snippit", None)
+        self.spatial_reference = data_json.get("spatialReference", None)
+        self.tags = data_json.get("tags", None)
+        self.thumbnail = data_json.get("thumbnail", None)
+        self.title = data_json.get("title", None)
+        self.type_ = data_json.get("type", None)
+        self.type_keywords = data_json.get("typeKeywords", None)
+        self.url = data_json.get("url", None)
+
+    # def build_arcgis_request_data_dict(self, start_num: int = None) -> dict:
+    #     return {
+    #         'q': DatasetAGOL.OWNER,
+    #         'num': DatasetAGOL.RECORD_LIMIT,
+    #         'start': start_num,
+    #         'sortField': DatasetAGOL.SORT_FIELD,
+    #         'f': 'json'
+    #     }
 
     @staticmethod
     def request_all_data_catalog_results(url: str) -> list:
@@ -56,7 +138,7 @@ class DatasetAGOL:
                 results = resp_json.get("results", {})
                 start_number = resp_json.get("nextStart", None)
                 master_list_of_dicts.extend(results)
-                print(f"Start Number: {start_number}")
+                # print(f"Start Number: {start_number}")
                 # number_of_records_returned = len(results)
                 # request_cycle_record_count += number_of_records_returned
                 # total_record_count += number_of_records_returned
