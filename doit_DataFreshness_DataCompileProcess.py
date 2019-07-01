138178e0b738ea1da774f42401e894f1904ab0c2
CJuice <CJuice@users.noreply.github.com>
2019-07-01 10:01:35 -0400

    refactoring

--- doit_DataFreshness_DataCompileProcess.py
+++ doit_DataFreshness_DataCompileProcess.py
@@ -4,11 +4,13 @@
 
 
 def main():
-    import os
-    import pandas as pd
     import datetime
     import json
+    import os
+    import pandas as pd
 
+    # VARIABLES
+    # NOT-DERIVED
     agol_dataframe_excel = None
     agol_excel_file_name = "AGOL_data_freshness.xlsx"
     agol_json_file_name = "AGOL_data_freshness.json"
@@ -22,17 +24,18 @@ def main():
     null_integer_value = -9999
     null_url_value = "https://N.U.LL"
     null_value_column_fillna_specs = {"Link": null_url_value,
-                                    "Source URL": null_url_value,
-                                    "Date of Most Recent Data Change": null_datetime_value,
-                                    "Days Since Last Data Update": null_integer_value,
-                                    "Date of Most Recent View Change in Data or Metadata": null_datetime_value,
-                                    "Days Since Last View Update": null_integer_value,
-                                    "Number of Rows": null_integer_value,
-                                    }
+                                      "Source URL": null_url_value,
+                                      "Date of Most Recent Data Change": null_datetime_value,
+                                      "Days Since Last Data Update": null_integer_value,
+                                      "Date of Most Recent View Change in Data or Metadata": null_datetime_value,
+                                      "Days Since Last View Update": null_integer_value,
+                                      "Number of Rows": null_integer_value}
 
+    # DERIVED
     combined_data_file_name_excel = f"{data_file_dir}\dataFreshness_{datetime.datetime.now().strftime('%Y_%m_%d')}.xlsx"
     combined_data_file_name_json = f"{data_file_dir}\{compiled_json_file_name}"
 
+    # FUNCTIONALITY
     # Excel Portion. The output is pushed up to Socrata Data Freshness report dataset by an FME job
     for dirname, dirs, files in os.walk(data_file_dir):
         for file in files:
@@ -43,17 +46,16 @@ def main():
 
     master_data_freshness_df_excel = pd.concat([agol_dataframe_excel, socrata_dataframe_excel])
 
-    # Unsure why NULL values are dropped on read but have to fill before exporting.
+    # Unsure why NULL values are dropped on read, some setting I'm missing, but have to fill before exporting.
     #   Performing two rounds of filling. First round hits specific fields that required special types.
     #   Second round fills any remaining empty fields with text NULL value.
     master_data_freshness_df_excel.fillna(value=null_value_column_fillna_specs, inplace=True)  # Targeted columns
     master_data_freshness_df_excel.fillna(value=null_string, inplace=True)  # All others (text fields)
-    print(master_data_freshness_df_excel.info())
-
     master_data_freshness_df_excel.to_excel(excel_writer=combined_data_file_name_excel,
                                             index=False)
+    print(master_data_freshness_df_excel.info())
 
-    # JSON Portion
+    # JSON Portion of the process
     for dirname, dirs, files in os.walk(data_file_dir):
         for file in files:
             if file == agol_json_file_name:
@@ -62,8 +64,12 @@ def main():
             elif file == socrata_json_file_name:
                 with open(os.path.join(dirname, file), 'r') as socrata_handler:
                     socrata_json = json.loads(socrata_handler.read())
-        break # Just making sure
+        break  # Just making sure
+
+    # Add the two realms together for a unified output
     combined_json_contents = agol_json + socrata_json
+
+    # Write the compiled output json file
     with open(combined_data_file_name_json, 'w') as compiled_handler:
         compiled_handler.write(json.dumps(combined_json_contents))
 
